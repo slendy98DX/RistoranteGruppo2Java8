944e35de13bd9ad6cb63dc514a18520d35f59433
M19732004 <mariacoman73@gmail.com>
2022-11-17 17:30:13 +0100

    add Dolci

--- src/Start.java
+++ src/Start.java
@@ -6,16 +6,25 @@ public class Start {
 
         menu.printRestaurantDetails();
 
-        Bevanda acqua = new Bevanda("Ferrarelle",false,2.5,false);
+        Bevanda acqua = new Bevanda("Ferrarelle", false, 2.5, false);
         Bevanda vino = new Bevanda("Tavernello", false, 5.6, true);
-        Bevanda analcolico = new Bevanda("Coca-cola", false,3.5, false);
+        Bevanda analcolico = new Bevanda("Coca-cola", false, 3.5, false);
 
-        System.out.println("Lista bevande:\n"+acqua.printName()+" Prezzo: "+acqua.printPrice()+" €"+"\n"+vino.printName()+" Prezzo: "+vino.printPrice()+" €"+"\n"+analcolico.printName()+" Prezzo: "+ analcolico.printPrice()+" €");
+        System.out.println("Lista bevande:\n" + acqua.printName() + " Prezzo: " + acqua.printPrice() + " €" + "\n" + vino.printName() + " Prezzo: " + vino.printPrice() + " €" + "\n" + analcolico.printName() + " Prezzo: " + analcolico.printPrice() + " €");
 
-        PrimoPiatto pasta = new PrimoPiatto("Amatriciana", true, 16,"Pasta,sugo,pancetta,pecorino" );
-        PrimoPiatto riso = new PrimoPiatto("Risotto funghi e salsiccia",true,20,"riso,salsiccia,funghi");
+        PrimoPiatto pasta = new PrimoPiatto("Amatriciana", true, 16, "Pasta,sugo,pancetta,pecorino");
+        PrimoPiatto riso = new PrimoPiatto("Risotto funghi e salsiccia", true, 20, "riso,salsiccia,funghi");
 
-        System.out.println("Primi Piatti:\n"+ pasta.printName()+ " Prezzo: "+pasta.printPrice()+" € Ingredienti: "+riso.printIngredients()+"\n"+riso.printName()+ " Prezzo: "+riso.printPrice()+" € Ingredienti: "+riso.printIngredients());
+        System.out.println("Primi Piatti:\n" + pasta.printName() + " Prezzo: " + pasta.printPrice() + " € Ingredienti: " + riso.printIngredients() + "\n" + riso.printName() + " Prezzo: " + riso.printPrice() + " € Ingredienti: " + riso.printIngredients());
+
+
+        Dolci dolci1 = new Dolci("Sorbetto al limone'-", "Sorbetti-", true, 5, "Succo di limone,Aqua,Zucchero,Limoncello,Albumi");
+        Dolci dolci2 = new Dolci("Tiramisu'", "Dessert", false, 10, "Uova,Mascarpone,Zucchero,Caffè,Rum");
+
+        System.out.println("Lista Dolci: \n" + dolci1.printName()+" Prezzo "+dolci1.printPrice()+" €"+" -Tipo di dolce: "+dolci1.printType()+"- Ingredienti: "+dolci1.printIngredientsDolci()+"\n"+ dolci2.printName()+" -Prezzo "+dolci2.printPrice()+" €"+" -Tipo di dolce "+dolci2.printType()+" -Ingredienti: "+dolci2.printIngredientsDolci());
 
     }
 }
+
+
+
