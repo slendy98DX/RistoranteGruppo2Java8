4fa1fa06b722ce342bb0b826074e177a6ed6a338
Michele Junior Cioffo <michelecioffo_98@hotmail.com>
2022-11-23 17:29:15 +0100

    Texts error

--- src/SecondoPiatto.java
+++ src/SecondoPiatto.java
@@ -1,35 +1,40 @@
 public class SecondoPiatto extends Portata {
-    public String sideDish;
 
 
-    private String ingredients;
+
+    String sideDish;
+
+
+
+    String ingredients;
+
 
     public SecondoPiatto(String name, double priceEuros, String sideDish, String ingredients) {
         super(name, priceEuros);
         this.sideDish = sideDish;
-        this.setIngredients(ingredients);
-    }
+        this.ingredients = ingredients;
 
-    public String getSideDish() {
-        return sideDish;
-    }
-
-    public void setSideDish(String sideDish) {
-        this.sideDish = sideDish;
     }
 
 
-    public String getIngredients() {
+        public String getSideDish() {
+            return sideDish;
+        }
+
+        public void setSideDish(String sideDish) {
+            this.sideDish = sideDish;
+        }
+
+        public String getIngredients() {
         return ingredients;
     }
-
-    public void setIngredients(String ingredients) {
+    public void setIngredientsSecond(String ingredients) {
         this.ingredients = ingredients;
     }
-
     @Override
     public void printPortataDetails() {
         System.out.printf("Nome: %s Prezzo: %.2f€ Contorno: %s Ingredienti: %s%n", getName(), getPriceEuros(), getSideDish(), getIngredients());
-
     }
 }
+
+
