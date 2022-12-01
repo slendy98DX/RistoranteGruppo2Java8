499255c917e5153ddf30d8cc6aac006f0810d5b7
Alessio Pollina <alexpollina@libero.it>
2022-12-01 18:53:35 +0100

    Modified Second Plates in all menus

--- src/Ristorante.java
+++ src/Ristorante.java
@@ -49,13 +49,8 @@ public class Ristorante{
             case CARNE:
                 System.out.println("MENU' DI CARNE");
                 listaCarne.add(new Bevanda("Acqua",2.5,""));
-<<<<<<< HEAD
-                listaCarne.add(new Bevanda("Tavernello",5.6,"uva"));
-                listaCarne.add(new Bevanda("Coca-cola",3.5,"zucchero, caffè"));
-=======
                 listaCarne.add(new Bevanda("Tavernello",5.6,""));
                 listaCarne.add(new Bevanda("Coca-cola",3.5,""));
->>>>>>> origin/develop
                 listaCarne.add(new PrimoPiatto("Amatriciana",  16, "Pasta,sugo,pancetta,pecorino"));
                 listaCarne.add(new PrimoPiatto("Risotto funghi e salsiccia",  20, "riso,salsiccia,funghi"));
                 listaCarne.add(new SecondoPiatto("Bistecca", 18.5, "Patate Fritte","Carne di Manzo, Limone, Aromi"));
