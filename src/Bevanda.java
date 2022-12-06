04dc9b267e7245773ada93005061b0aa5c422168
slendy98DX <marco.setaro93@gmail.com>
2022-12-06 16:52:22 +0100

    Refactoring classe Bevanda

--- src/Bevanda.java
+++ src/Bevanda.java
@@ -1,11 +1,23 @@
 public class Bevanda extends Portata{
 
-    public Bevanda(String name, double priceEuros, String ingridients) {
-        super(name, priceEuros, ingridients);
+    private boolean isAlcolic;
+
+
+    public Bevanda(TypeEnum typeEnum, String name, double priceEuros, String ingridients, boolean isAlcolic) {
+        super(typeEnum, name, priceEuros, ingridients);
+        this.isAlcolic = isAlcolic;
+    }
+
+    public boolean isAlcolic() {
+        return isAlcolic;
+    }
+
+    public void setAlcolic(boolean alcolic) {
+        isAlcolic = alcolic;
     }
 
     @Override
-    public String printPortataDetails() {
-        return String.format("Nome: %s Prezzo: %.2f€ Ingredienti: %s", getName(), getPriceEuros(),getIngridients());
+    public void printPortataDetails() {
+        System.out.printf("Tipo di portata: %s Nome: %s Prezzo: %.2f € Ingredienti: %s E' alcolico?: %b", getTypeEnum(),getName(),getPriceEuros(),getIngridients(),isAlcolic());
     }
 }
