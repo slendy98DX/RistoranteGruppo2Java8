5ab38c5435418340a6ea27fa366d9ea616336af0
Alessio Pollina <alexpollina@libero.it>
2022-12-06 17:32:06 +0100

    Refactoring Restaurant Class

--- src/Ristorante.java
+++ src/Ristorante.java
@@ -50,18 +50,6 @@ public class Ristorante {
         System.out.printf("Nome: %s%nIndirizzo: %s%nMenù disponibili: %s%n", getRestaurantName(), getAddress(), getMenuType());
     }
 
-    /*
-        listaCarne.add(new PrimoPiatto("Amatriciana", 16, "Pasta,sugo,pancetta,pecorino"));
-        listaCarne.add(new PrimoPiatto("Risotto funghi e salsiccia", 20, "riso,salsiccia,funghi"));
-        listaCarne.add(new SecondoPiatto("Bistecca", 18.5, "Patate Fritte", "Carne di Manzo, Limone, Aromi"));
-        listaCarne.add(new SecondoPiatto("Pollo alla Cacciatora", 15.3, "Patate al Forno", "Pollo, Pomodoro, Spezie, Cipolla, Vino, Verdure"));
-        listaCarne.add(new Dolci("Tiramisu'", "Dessert", 10, "Uova,Mascarpone,Zucchero,Caffè,Rum"));
-        listaCarne.add(new Dolci("Sorbetto al limone", "Sorbetti", 5, "Succo di limone,Aqua,Zucchero,Limoncello,Albumi"));
-        listaCarne.forEach(portata -> {
-            System.out.println(portata.printPortataDetails());
-        });*/
-
-
     private List<Portata> getPortataList() {
         return portataList;
     }
