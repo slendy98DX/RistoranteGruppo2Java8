3614e775e0f80f4ffbadd56a627e1b7e9a1004f4
Alessio Pollina <alexpollina@libero.it>
2022-12-06 17:33:04 +0100

    Refactoring Added Primi Piatti e Secondi Piatti alla Lista

--- src/Start.java
+++ src/Start.java
@@ -10,13 +10,12 @@ public class Start {
         ristorante.addPortata(new Bevanda(TypeEnum.BEVANDA,"Acqua",2.5,"",false));
         ristorante.addPortata(new Bevanda(TypeEnum.BEVANDA,"Tavernello", 5.6, "",true));
         ristorante.addPortata(new Bevanda(TypeEnum.BEVANDA,"Coca-cola",3.5, "",false));
-        ristorante.addPortata(new Bevanda(TypeEnum.BEVANDA,"Coca-cola",3.5, "",false));
-        ristorante.addPortata(new Bevanda(TypeEnum.BEVANDA,"Coca-cola",3.5, "",false));
-        ristorante.addPortata(new Bevanda(TypeEnum.BEVANDA,"Coca-cola",3.5, "",false));
-        ristorante.addPortata(new Bevanda(TypeEnum.BEVANDA,"Coca-cola",3.5, "",false));
+        ristorante.addPortata(new PrimoPiatto(TypeEnum.PRIMO_PIATTO, "Risotto Funghi e Salsiccia", 13.3, "riso,salsiccia,funghi","Caldo"));
+        ristorante.addPortata(new PrimoPiatto(TypeEnum.PRIMO_PIATTO, "Amatriciana", 16, "Pasta,sugo,pancetta,pecorino", "Caldo"));
+        ristorante.addPortata(new SecondoPiatto(TypeEnum.SECONDO_PIATTO, "Pollo alla Cacciatora", 17.4, "Pollo, Pomodoro, Spezie, Cipolla, Vino, Verdure", "Patate al Forno", "Caldo"));
+        ristorante.addPortata(new SecondoPiatto(TypeEnum.SECONDO_PIATTO, "Bistecca", 16.5, "Carne di Manzo, Limone, Aromi", "Patate Fritte", "Caldo"));
         ristorante.addPortata(new Dolci(TypeEnum.DOLCE,"Tiramisu'",10,"Uova,Mascarpone,Zucchero,Caffè,Rum","Dessert"));
         ristorante.addPortata(new Dolci(TypeEnum.DOLCE, "Sorbetto al limone",5,"Succo di limone,Aqua,Zucchero,Limoncello,Albumi","Sorbetti"));
-
         ristorante.printMenuRistorante();
 
     }
