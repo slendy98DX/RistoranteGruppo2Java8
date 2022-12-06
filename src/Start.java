6840174ba9efaa79ba7c6331f3d6def4a5347248
M19732004 <mariacoman73@gmail.com>
2022-12-06 17:16:56 +0100

    Added Dolci to List

--- src/Start.java
+++ src/Start.java
@@ -10,6 +10,12 @@ public class Start {
         ristorante.addPortata(new Bevanda(TypeEnum.BEVANDA,"Acqua",2.5,"",false));
         ristorante.addPortata(new Bevanda(TypeEnum.BEVANDA,"Tavernello", 5.6, "",true));
         ristorante.addPortata(new Bevanda(TypeEnum.BEVANDA,"Coca-cola",3.5, "",false));
+        ristorante.addPortata(new Bevanda(TypeEnum.BEVANDA,"Coca-cola",3.5, "",false));
+        ristorante.addPortata(new Bevanda(TypeEnum.BEVANDA,"Coca-cola",3.5, "",false));
+        ristorante.addPortata(new Bevanda(TypeEnum.BEVANDA,"Coca-cola",3.5, "",false));
+        ristorante.addPortata(new Bevanda(TypeEnum.BEVANDA,"Coca-cola",3.5, "",false));
+        ristorante.addPortata(new Dolci(TypeEnum.DOLCE,"Tiramisu'",10,"Uova,Mascarpone,Zucchero,Caffè,Rum","Dessert"));
+        ristorante.addPortata(new Dolci(TypeEnum.DOLCE, "Sorbetto al limone",5,"Succo di limone,Aqua,Zucchero,Limoncello,Albumi","Sorbetti"));
 
         ristorante.printMenuRistorante();
 
